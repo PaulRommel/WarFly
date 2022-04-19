84947c491e79562f6078d38955caf0259bd5ec9e
Paul <paulrommel87@gmail.com>
2022-04-19 20:09:28 +0300

    10 Moving background

---
+++ WarFly/protocol GameBackgroundSpriteable + Extension.swift
@@ -0,0 +1,24 @@
+//
+//  GameBackgroundSpriteable + Extension.swift
+//  WarFly
+//
+//  Created by Павел Попов on 19.04.2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+protocol GameBackgroundSpriteable {
+    static func populate() -> Self
+    static func randomPoint() -> CGPoint
+}
+
+extension GameBackgroundSpriteable {
+    static func randomPoint() -> CGPoint {
+        let screen = UIScreen.main.bounds
+        let distribution = GKRandomDistribution(lowestValue: Int(screen.size.height) + 100, highestValue: Int(screen.size.height) + 200)
+        let y = CGFloat(distribution.nextInt())
+        let x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
+        return CGPoint(x: x, y: y)
+    }
+}
