544c7bc6b0be85c6a0c9c6bf54fa0b642ae398ec
Pavel Popov <Popov.Pavel.87@yandex.ru>
2022-05-14 16:54:21 +0300

    32 Pause scene

--- WarFly/Scenes/PauseScene.swift
+++ WarFly/Scenes/PauseScene.swift
@@ -1,26 +1,23 @@
 //
-//  MenuScene.swift
+//  PauseScene.swift
 //  WarFly
 //
-//  Created by Pavel Popov on 06.05.2022.
+//  Created by Pavel Popov on 14.05.2022.
 //
 
 import SpriteKit
 
-class MenuScene: SKScene {
+class PauseScene: SKScene {
+    
     override func didMove(to view: SKView) {
-        if !Assets.shared.isLoaded {
-            Assets.shared.preloadAssets()
-            Assets.shared.isLoaded = true
-        }
         
         self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
         
-        let header = SKSpriteNode(imageNamed: "header1")
+        let header = ButtonNode(titled: "pause", backgroundName: "header_background")
         header.position = CGPoint(x: self.frame.midX, y: self.frame.midY + 150)
         self.addChild(header)
         
-        let titles = ["play", "options", "best"]
+        let titles = ["restart", "options", "resume"]
         
         for (index, title) in titles.enumerated() {
             let button = ButtonNode(titled: title, backgroundName: "button_background")
@@ -35,7 +32,7 @@ class MenuScene: SKScene {
         let location = touches.first!.location(in: self)
         let node = self.atPoint(location)
         
-        if node.name == "play" {
+        if node.name == "restart" {
             let transition = SKTransition.crossFade(withDuration: 1.0)
             let gameScene = GameScene(size: self.size)
             gameScene.scaleMode = .aspectFit
