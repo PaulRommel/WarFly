6ac1f27ca9e2d179842b7195e340a42a7a8a16f1
Pavel Popov <Popov.Pavel.87@yandex.ru>
2022-05-18 21:51:37 +0300

    37 Collision explosion

--- WarFly/Scenes/GameScene.swift
+++ WarFly/Scenes/GameScene.swift
@@ -180,11 +180,31 @@ extension GameScene: SKPhysicsContactDelegate {
     
     func didBegin(_ contact: SKPhysicsContact) {
         
+        let explosion = SKEmitterNode(fileNamed: "EnemyExplosion")
+        let contactPoint = contact.contactPoint
+        explosion?.position = contactPoint
+        let waitForExplosionAction = SKAction.wait(forDuration: 1.0)
+        
         let contactCategory: BitMaskCategory = [contact.bodyA.category, contact.bodyB.category]
         switch contactCategory {
         case [.enemy, .player]: print("enemy vs player")
+            if contact.bodyA.node?.name == "sprite" {
+                contact.bodyA.node?.removeFromParent()
+            } else {
+                contact.bodyB.node?.removeFromParent()
+            }
+            addChild(explosion!)
+            self.run(waitForExplosionAction, completion: {
+                explosion?.removeFromParent()
+            })
         case [.powerUp, .player]: print("powerUp vs player")
         case [.enemy, .shot]: print("enemy vs shot")
+            contact.bodyA.node?.removeFromParent()
+            contact.bodyB.node?.removeFromParent()
+            addChild(explosion!)
+            self.run(waitForExplosionAction, completion: {
+                explosion?.removeFromParent()
+            })
         default: preconditionFailure("Unable to detect collision category")
         }
     }
